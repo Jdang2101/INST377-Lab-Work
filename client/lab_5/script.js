f3264aeec0135f59153a141ef45ec711096a383c
Jimmy <jdang123@terpmail.umd.edu>
2023-03-14 17:20:47 -0400

    5th

--- client/lab_5/script.js
+++ client/lab_5/script.js
@@ -58,8 +58,8 @@ async function mainEvent() { // the async keyword means we can make API requests
   filterButton.addEventListener('click', (event) => {
     console.log('clicked FilterButton');
 
-    const FormData = new FormData(mainForm);
-    const formProps = Object.fromEntries(FormData);
+    const formData = new FormData(mainForm);
+    const formProps = Object.fromEntries(formData);
 
     console.log(formProps);
   })
